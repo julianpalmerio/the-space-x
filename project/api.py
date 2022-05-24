401160a726b756bf6b0ffe9e62d5324d3690a5b6
julianpalmerio <julianpalmerio@hotmail.com>
2022-05-23 23:34:41 -0300

    feature: add api_v1 blueprint

---
+++ project/api.py
@@ -0,0 +1,2 @@
+from flask import Blueprint
+blueprint_api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
--- project/app.py
+++ project/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(blueprint_api_v1)
     return app
 
 
