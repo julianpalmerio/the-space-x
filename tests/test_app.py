1fb8ec42f1ce4fcf0388cd1fdd60f8dc9a73dbf5
julianpalmerio <julianpalmerio@hotmail.com>
2022-05-26 22:58:44 -0300

    test: add tests for the bug card and a missing test for the issue card

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,8 +18,44 @@ def test_issue_card_fail_miss_title(client):
 
 def test_issue_card_fail_miss_description(client):
     """
+    Test if the request fails if it receives the issue type and title.
+    """
+    data_json = {"type": "issue",  "title": "Send message"}
+    response = client.post("api/v1/cards/card", json=data_json)
+    assert response.status_code == 400
+
+
+def test_issue_card_fail_miss_description_and_title(client):
+    """
     Test if the request fails if it receives only the issue type.
     """
     data_json = {"type": "issue"}
+    response = client.post("api/v1/cards/card", json=data_json)
+    assert response.status_code == 400
+
+
+def test_bug_card_ok(client):
+    """
+    Test if the request is ok if it receives the bug type and description.
+    """
+    data_json = {"type": "bug", "description": "Cockpit is not depressurizing correctly"}
+    response = client.post("api/v1/cards/card", json=data_json)
+    assert response.status_code == 200
+
+
+def test_bug_card_fail_miss_description(client):
+    """
+    Test if the request fails if it receives the bug type.
+    """
+    data_json = {"type": "bug"}
+    response = client.post("api/v1/cards/card", json=data_json)
+    assert response.status_code == 400
+
+
+def test_bug_card_fail_description_and_title(client):
+    """
+    Test if the request fails if it receives the bug type.
+    """
+    data_json = {"type": "bug", "title": "some_title", "description": "Cockpit is not depressurizing correctly"}
     response = client.post("api/v1/cards/card", json=data_json)
     assert response.status_code == 400
