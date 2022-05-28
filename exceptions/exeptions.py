50aff25fe727d3bb98bec5af43006f74053e80fc
julianpalmerio <julianpalmerio@hotmail.com>
2022-05-27 21:52:35 -0300

    refactor: adds the trello connector and implements it to remove the logic from the card endpoint. It also adds custom exceptions used in it.

---
+++ exceptions/exeptions.py
@@ -0,0 +1,14 @@
+class AppErrorBaseClassExeption(Exception):
+    pass
+
+
+class GetMembersException(AppErrorBaseClassExeption):
+    pass
+
+
+class GetLabelsException(AppErrorBaseClassExeption):
+    pass
+
+
+class PostCardException(AppErrorBaseClassExeption):
+    pass
